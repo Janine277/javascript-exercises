d0889b5affaa6db3d180aae88db00117a1d1ea98
Janine <gadsgalore1@gmail.com>
2024-07-11 15:28:19 -0600

    corrected all code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+const divByfour = year % 4 ==0;
+const century= year % 100 ==0;
+const divBy400= year % 400 ==0;
 
+if (divByfour &&
+(!century||divBy400))
+{return true;}
+else{
+return false;}
 };
 
 // Do not edit below this line
